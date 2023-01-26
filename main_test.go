c34a4a035c53399a916af7fbaefab10b019525fd
Pedro Brügger <pedromurilobrugger@hotmail.com>
2023-01-26 10:51:51 -0300

    Fazendo mais alguns testes

--- main_test.go
+++ main_test.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/BruggerPedro/gin-api-rest/controllers"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
 func SetupDasRotasDeTeste() *gin.Engine {
@@ -11,7 +17,16 @@ func SetupDasRotasDeTeste() *gin.Engine {
 	return rotas
 }
 
-func TestFalha(t *testing.T) {
-	t.Fatalf("Teste falhou de propósito")
+func TestVerificaStatusCode(t *testing.T) {
+	r := SetupDasRotasDeTeste()
+	r.GET("/:nome", controllers.Saudacao)
+	req, _ := http.NewRequest("GET", "/pedro", nil)
+	resposta := httptest.NewRecorder() // salva todas as informacoes de resposta do http
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code, "Deveriam ser iguais")
+	mockDaResposta := `{"API diz:":"E ai pedro, tudo beleza?"}`
+	respostaBody, _ := ioutil.ReadAll(resposta.Body)
+	assert.Equal(t, mockDaResposta, string(respostaBody))
+	fmt.Println(string(respostaBody))
+	fmt.Println(mockDaResposta)
 }
-  
