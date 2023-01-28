ec41d241e116bd179089809875a968fd92907468
Pedro Brügger <pedromurilobrugger@hotmail.com>
2023-01-28 10:21:48 -0300

    Curso finalizado

--- main_test.go
+++ main_test.go
@@ -1,18 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 
 	"github.com/BruggerPedro/gin-api-rest/controllers"
+	"github.com/BruggerPedro/gin-api-rest/database"
+	"github.com/BruggerPedro/gin-api-rest/models"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
+var ID int
+
 func SetupDasRotasDeTeste() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
 	rotas := gin.Default()
 	return rotas
 }
@@ -24,9 +31,96 @@ func TestVerificaStatusCode(t *testing.T) {
 	resposta := httptest.NewRecorder() // salva todas as informacoes de resposta do http
 	r.ServeHTTP(resposta, req)
 	assert.Equal(t, http.StatusOK, resposta.Code, "Deveriam ser iguais")
-	mockDaResposta := `{"API diz:":"E ai pedro, tudo beleza?"}`
+	mockDaResposta := `{"API diz:":"E aí pedro, tudo beleza?"}`
 	respostaBody, _ := ioutil.ReadAll(resposta.Body)
 	assert.Equal(t, mockDaResposta, string(respostaBody))
-	fmt.Println(string(respostaBody))
-	fmt.Println(mockDaResposta)
+	// fmt.Println(string(respostaBody))
+	// fmt.Println(mockDaResposta)
+}
+
+func CriaAlunoMock() {
+	aluno := models.Aluno{Nome: "Nome Teste", CPF: "12345678909", RG: "12345678"}
+	database.DB.Create(&aluno)
+	ID = int(aluno.ID)
+}
+
+func DeletaAlunoMock() {
+	var aluno models.Aluno
+	database.DB.Delete(&aluno, ID)
+}
+
+func TestListandoTodosOsAlunos(t *testing.T) {
+	database.ConnectDB()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos", controllers.ExibeTodosOsAlunos)
+	req, _ := http.NewRequest("GET", "/alunos", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code)
+	// fmt.Println(resposta.Body)
+}
+
+func TestBuscaAlunoPorCPFHandler(t *testing.T) {
+	database.ConnectDB()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := SetupDasRotasDeTeste()
+	r.GET("alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
+	req, _ := http.NewRequest("GET", "/alunos/cpf/12345678909", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code)
+}
+
+func TestBuscaAlunoPorIDHandler(t *testing.T) {
+	database.ConnectDB()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos/:id", controllers.BuscaAlunoPorID)
+	pathDaBusca := "/alunos/" + strconv.Itoa(ID)
+	req, _ := http.NewRequest("GET", pathDaBusca, nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	var alunoMock models.Aluno
+	json.Unmarshal(resposta.Body.Bytes(), &alunoMock)
+	assert.Equal(t, "Nome Teste", alunoMock.Nome)
+	assert.Equal(t, "12345678909", alunoMock.CPF)
+	assert.Equal(t, "12345678", alunoMock.RG)
+	assert.Equal(t, http.StatusOK, resposta.Code)
+}
+
+func TestDeletaAlunoHandler(t *testing.T) {
+	database.ConnectDB()
+	CriaAlunoMock()
+	r := SetupDasRotasDeTeste()
+	r.DELETE("/alunos/:id", controllers.DeletaAluno)
+	pathDeBusca := "/alunos/" + strconv.Itoa(ID)
+	req, _ := http.NewRequest("DELETE", pathDeBusca, nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code)
+}
+
+// REVISAR POIS ESTÁ COM BUG
+func TestEditaAlunoHandler(t *testing.T) {
+	database.ConnectDB()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := SetupDasRotasDeTeste()
+	r.PATCH("/alunos/:id", controllers.EditaAluno)
+	aluno := models.Aluno{Nome: "Nome Teste", CPF: "47123456789", RG: "12345670"}
+	valorJson, _ := json.Marshal(aluno)
+	pathParaEditar := "/alunos/" + strconv.Itoa(ID)
+	req, _ := http.NewRequest("PATCH", pathParaEditar, bytes.NewBuffer(valorJson))
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	var alunoMockAtualizado models.Aluno
+	json.Unmarshal(resposta.Body.Bytes(), &alunoMockAtualizado)
+	assert.Equal(t, "47123456789", alunoMockAtualizado.CPF)
+	assert.Equal(t, "12345670", alunoMockAtualizado.RG)
+	assert.Equal(t, "Nome Teste", alunoMockAtualizado.Nome)
+	assert.Equal(t, http.StatusOK, resposta.Code)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -7,12 +7,17 @@ import (
 
 func HandleRequests() {
 	r := gin.Default()
-	r.GET("/alunos", controllers.ExibeTodosOsAlunos)
+	r.LoadHTMLGlob("templates/*") // Carrega as páginas HTML
+	r.Static("/assets", "./assets")
 	r.GET("/:nome", controllers.Saudacao)
-	r.POST("/alunos", controllers.CriaNovoAluno)
+	r.GET("/alunos", controllers.ExibeTodosOsAlunos)
 	r.GET("/alunos/:id", controllers.BuscaAlunoPorID)
+	r.POST("/alunos", controllers.CriaNovoAluno)
 	r.DELETE("/alunos/:id", controllers.DeletaAluno)
-	r.PATCH("alunos/:id", controllers.EditaAluno)
-	r.GET("alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
+	r.PATCH("/alunos/:id", controllers.EditaAluno)
+	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
+	r.GET("/alunos/", controllers.BuscaAlunoPorCPF)
+	r.GET("/index", controllers.ExibePaginaIndex)
+	r.NoRoute(controllers.RotaNaoEncontrada)
 	r.Run() //Caso queira rodar em outra por que não seja a padrão(:8080), é só colocar como primeiro argumento ":5000"
 }
